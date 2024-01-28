f846b7059612453f804f8d6adb226f4c3d0c2ea6
mookor <andreymazko147@gmail.com>
2024-01-28 15:56:44 +0700

    removed the crutch solutions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="easy_udp",
-    version="0.1.4",
+    version="0.1.5",
     packages=find_packages(),
     install_requires=["numpy"],
     description="Easy UDP communication library",
